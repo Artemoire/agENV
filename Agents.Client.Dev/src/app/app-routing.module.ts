28661d901f7b726476b1e51d445acf9ae88004e2
Dejan Tot <dejan.tot@gmail.com>
2018-05-03 09:18:24 +0200

    UI progress

--- Agents.Client.Dev/src/app/app-routing.module.ts
+++ Agents.Client.Dev/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: "", component: LoginComponent }
+  { path: "", component: HomeComponent },
+  { path: "register", component: RegisterComponent }
 ];
 
 @NgModule({
--- Agents.Client.Dev/src/app/app.module.ts
+++ Agents.Client.Dev/src/app/app.module.ts
@@ -8,12 +8,18 @@ import { MaterialModule } from './modules/material/material.module';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { InboxComponent } from './components/inbox/inbox.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    InboxComponent
   ],
   imports: [
     BrowserModule,
