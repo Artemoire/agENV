8fda49e6ddecfb6d1e828b2700a3aacbf7e3a81c
unknown <isidoraxaleksic@gmail.com>
2018-05-04 02:39:49 +0200

    JMS in progress

--- Agents.UserApp/src/controller/GroupController.java
+++ Agents.UserApp/src/controller/GroupController.java
@@ -1,19 +1,17 @@
 package controller;
 
-import java.util.List;
-
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
-import models.Group;
 import services.GroupService;
 
 @Path("/groups")
@@ -27,27 +25,29 @@ public class GroupController {
 
 	@POST
 	@Path("/{groupAdminId}/{groupName}")
-	public void createGroup(@PathParam("groupAdminId") String groupAdminId, @PathParam("groupName") String groupName) {
+	public Response createGroup(@PathParam("groupAdminId") String groupAdminId, @PathParam("groupName") String groupName) {
 		groupService.createGroup(groupAdminId, groupName);
+		return Response.ok().entity("created group").build();
 	}
 	
 	@DELETE
 	@Path("/{groupAdminId}/{groupId}")
-	public void deleteGroup(@PathParam("groupAdminId") String groupAdminId, @PathParam("groupId") String groupId) {
+	public Response deleteGroup(@PathParam("groupAdminId") String groupAdminId, @PathParam("groupId") String groupId) {
 		groupService.deleteGroup(groupAdminId, groupId);
+		return Response.ok().entity("deleted group").build();
 	}
 	
 	@POST
 	@Path("/addNewUsersToGroup/{groupAdminId}/{newUserId}/{groupId}")
-	public void addNewUser(@PathParam("groupAdminId") String groupAdminId, @PathParam("newUserId") String newUserId, @PathParam("groupId") String groupId) {
+	public Response addNewUser(@PathParam("groupAdminId") String groupAdminId, @PathParam("newUserId") String newUserId, @PathParam("groupId") String groupId) {
 		groupService.addNewUser(groupAdminId, newUserId, groupId);
-
+		return Response.ok().entity("added new user").build();
 	}
 
 	@DELETE
 	@Path("/deleteUsersFromGroup/{groupAdminId}/{newUserId}/{groupId}")
-	public void deleteUser(@PathParam("groupAdminId") String groupAdminId, @PathParam("newUserId") String newUserId, @PathParam("groupId") String groupId) {
+	public Response deleteUser(@PathParam("groupAdminId") String groupAdminId, @PathParam("newUserId") String newUserId, @PathParam("groupId") String groupId) {
 		groupService.deleteUser(groupAdminId, newUserId, groupId);
-
+		return Response.ok().entity("deleted user").build();
 	}
 }
