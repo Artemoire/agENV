56bfecc8a2510f9c404ed2d78cdadcf195274c6d
Dejan Tot <dejan.tot@gmail.com>
2018-07-02 20:13:23 +0200

    Handshake fix

--- agENV.EJB/ejbModule/com/agenv/services/HandshakeMasterService.java
+++ agENV.EJB/ejbModule/com/agenv/services/HandshakeMasterService.java
@@ -21,22 +21,23 @@ public class HandshakeMasterService {
 		// Step 0: payload is always arraylist
 		List<Node> payload = new ArrayList<>();
 		payload.add(node);
-		
+
 		// Step 1: Notify nodes of new node
-		for (Node myNode : envBean.getNodes()) {
+		for (Node myNode : envBean.getNodes().subList(1, envBean.getNodes().size() - 1)) {
 			ClientBuilder.newClient().target("http://" + myNode.getCenter().getAddress() + "/node").request().async()
 					.post(Entity.json(payload));
 		}
+		
 		// Step 2: Add node to myself
 		envBean.newNodeRegistered(node);
-					
+
 		// Step 3: send my nodes to new node
-		ClientBuilder.newClient().target("http://"+node.getCenter().getAddress()+"/node").request().post(
-				Entity.json(envBean.getNodes()));
-		
+		ClientBuilder.newClient().target("http://" + node.getCenter().getAddress() + "/node").request()
+				.post(Entity.json(envBean.getNodes()));
+
 		// Step 4: send running agents to new node
-		ClientBuilder.newClient().target("http://"+node.getCenter().getAddress()+"/agents/running").request().post(
-				Entity.json(envBean.getAgents()));		
+		ClientBuilder.newClient().target("http://" + node.getCenter().getAddress() + "/agents/running").request()
+				.post(Entity.json(envBean.getAgents()));
 	}
 
 }
